65021d253cc6fdb9f57609f58017f703939ea083
duysqubix <duys@qubixat.com>
2021-07-05 22:52:38 +0000

    commit

--- query.sql
+++ query.sql
@@ -6,5 +6,5 @@ sys.coords_z,
 codex."Codex Entry"
 FROM airbyte_codex_dump codex
 INNER JOIN db_system sys ON sys.id64=codex."System Address / ID64"
-WHERE codex."Codex Entry" = 'Guardian Ruins' 
+WHERE codex."Codex Entry" like 'Guardian%' 
 and (abs(sys.coords_x) > 300.0 and abs(sys.coords_y) > 500.0 and abs(sys.coords_z) > 500.0)
